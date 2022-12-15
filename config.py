8f4a1491ffd9e12ea7644cf08b7fd3b5aff7c8bb
PIRATtt <77304210+PIRATtt@users.noreply.github.com>
2022-12-15 11:21:53 +0300

    for docker-compose

---
+++ config.py
@@ -0,0 +1,4 @@
+host="std-mysql.ist.mospolytech.ru"
+user="std_1929_test"
+password="std_1929_test"
+db_name="std_1929_test"
---
+++ name.py
@@ -0,0 +1,11 @@
+import pymysql
+from config import host, user, password, db_name
+
+try:
+    connection = pymysql.connect(
+        host=host, user=user, password=password, database=db_name, cursorclass=pymysql.cursors.DictCursor
+        )
+    print('Ok, bro!')
+except Exception as ex:
+    print('Failed to connect!')
+    print(ex)
